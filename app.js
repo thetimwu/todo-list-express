913f8472f9513cb713c39f66d346050b06159dcf
tim wu <timwuuk@gmail.com>
2020-01-09 23:33:58 +0000

    adding item function and css style

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+let items = ["first item"];
+
 app.get("/", (req, res) => {
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
 
-  switch (currentDay) {
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Fridayday";
-      break;
-    case 6:
-    case 0:
-      day = "Weekend";
-      break;
-    default:
-      console.log(day);
-  }
+  let currentDay = today.toLocaleDateString("en-us", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: currentDay, items: items });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.newItem);
+  items.push(req.body.newItem);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
